d9a6b69bd8ca908c32cfa7be34c9d25c0d74f67a
Staslker <staslker@gmail.com>
2016-03-04 10:02:27 +0200

    edit update func and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
@@ -7,14 +8,46 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
         flash[:success] = "wellcome to the alpha blog #{@user.username}"
-        redirect_to articles_path
+        redirect_to articles_path 
     else
-      rendr'new' 
+      render'new' 
     end
   end
+
+def edit
+    @user = User.find(params[:id])
+
+  end
+
+
+def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+
+    else
+      render 'edit'
+    end
+
+end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-end
+
+
+
+
+  
+  
+
+  
+  
+  end
+
+
+
+
+  
